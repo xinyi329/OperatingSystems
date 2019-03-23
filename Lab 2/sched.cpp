79d608538a1a232d6007231f4075e54e39b8e855
Xinyi Liu <xyliu329@gmail.com>
2019-03-23 11:46:45 -0400

    Finish reading files and add to event queue.

--- Lab 2/sched.cpp
+++ Lab 2/sched.cpp
@@ -10,7 +10,7 @@
 #include <iomanip>
 #include <cstring>
 #include <string>
-#include <queue>
+#include <list>
 
 using namespace std;
 
@@ -78,9 +78,9 @@ typedef enum {
 	TRANS_TO_DONE
 } transition_t;
 
-class process {
-public:
+struct process {
 	// process attributes - input
+	int PID;			// process ID
 	int AT;				// arrival time
 	int TC;				// total CPU time
 	int CB;				// CPU burst
@@ -93,37 +93,70 @@ public:
 	int IT;				// IO time
 	int CW;				// CPU waiting time
 
-	// constructor
-	process() {};
-	process(int arrival_time, int total_CPU_time, int CPU_burst, int IO_burst, int priority) {
+	process(int process_id, int arrival_time, int total_CPU_time, int CPU_burst, int IO_burst, int priority) {
+		PID = process_id;
 		AT = arrival_time;
 		TC = total_CPU_time;
 		CB = CPU_burst;
 		IO = IO_burst;
 		PRIO = priority;
 	};
-
-	// destructor
-	~process() {};
 };
 
-class event {
-public:
+struct event {
 	// event attributes
 	int TS;				// time stamp
 	process *PROC;		// process
 	transition_t TRANS;	// transition
 
-	// constructor
-	event() {};
 	event(int timestamp, process *p, transition_t transition) {
 		TS = timestamp;
 		PROC = p;
 		TRANS = transition;
 	};
+};
+
+class event_queue {
+private:
+	list<event *> q;	// use list rather than queue for iteration use
+
+public:
+	event *get_event() {
+		if(q.empty()) {
+			return nullptr;
+		}
+		else {
+			event *evt = q.front();
+			q.pop_front();
+			return evt;
+		}
+	};
+
+	void put_event(event *evt) {
+		q.push_back(evt);
+	};
+
+	int get_next_event_time() {
+		if(q.empty()) {
+			return -1;
+		}
+		else {
+			return q.front()->TS;
+		}
+	};
+
+	// for tests
+	void print() {
+		for(list<event *>::iterator iter = q.begin(); iter != q.end(); iter++) {
+			cout << (*iter)->PROC->PID << "\t" << (*iter)->PROC->AT << "\t" << (*iter)->PROC->TC << "\t" << (*iter)->PROC->CB << "\t" << (*iter)->PROC->IO << "\n";
+		}
+	};
+
+	// constructor
+	event_queue() {};
 
 	// destructor
-	~event() {};
+	~event_queue() {};
 };
 
 class scheduler {
@@ -171,13 +204,12 @@ public:
 	process *get_next_process();
 };
 
-queue<event *> event_queue;
-
 int main(int argc, char *argv[]) {
-	// read command - get scheduler type
-	int c;
 	string scheduler_type = "";
 	bool verbose = false;
+
+	// read command - get output/scheduler type
+	int c;
 	while(1) {
 		c = getopt(argc, argv, "s:vte");
 		if(c == -1)
@@ -198,10 +230,37 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
+	event_queue *eq = new event_queue();
+
 	// read files
 	char *inputfile = argv[optind];
 	char *randfile = argv[optind + 1];
 	myrandom r(randfile);
+	ifstream input;
+	input.open(inputfile);
+	if(!input.is_open()) {
+		exit(0);
+	}
+	int process_id = 0;
+	while(!input.eof()) {
+		int arrival_time;
+		input >> arrival_time;
+		if(input.eof()) {
+			break;
+		}
+		int total_CPU_time;
+		input >> total_CPU_time;
+		int CPU_burst;
+		input >> CPU_burst;
+		int IO_burst;
+		input >> IO_burst;
+		process *p = new process(process_id, arrival_time, total_CPU_time, CPU_burst, IO_burst, r.get_random(4) - 1);
+		event *e = new event(arrival_time, p, TRANS_TO_READY);
+		eq->put_event(e);
+		process_id++;
+	}
+
+	eq->print();
 
 	return 0;
 }
